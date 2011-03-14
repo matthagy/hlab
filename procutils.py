a29b2b74bf1c954545180b866f1b5f2089006713
Matt Hagy <hagy@gatech.edu>
2011-03-14 13:09:36 -0400

    Created PidRecorder class

--- procutils.py
+++ procutils.py
@@ -4,8 +4,10 @@
 from __future__ import division
 from __future__ import absolute_import
 
+import time
 import errno
 import subprocess
+from socket import gethostname
 
 from .pathutils import FilePath
 
@@ -26,7 +28,9 @@ def which(proc, which_path='/usr/bin/which'):
     return paths[0]
 
 def run_program(prog, *args, **kwds):
-    p = do_while_interrupted(lambda : subprocess.Popen((which(prog),) + args, **kwds))
+    prog = which(prog)
+    p = do_while_interrupted(lambda : subprocess.Popen((prog,) + args, **kwds))
+    run_program_notify(p, prog, args, kwds)
     r = do_while_interrupted(lambda : p.wait())
     if r:
         raise subprocess.CalledProcessError(r, prog)
@@ -39,3 +43,48 @@ def do_while_interrupted(func):
         except (OSError,IOError), e:
             if e.errno != errno.EINTR:
                 raise
+
+run_program_notify_hook = []
+
+def run_program_notify(proc, prog, args, kwds):
+    for fun in run_program_notify_hook:
+        func(proc, prog, args, kwds)
+
+def add_run_program_notify(func):
+    assert callable(func)
+    run_program_notify_hook.append(func)
+
+
+class PidRecorder(object):
+
+    def __init__(self, pid_path):
+        self.pid_path = pid_path
+        self.fp = None if self.pid_path is None else open(self.pid_path, 'w')
+        self.msg('host %s', gethostname())
+        add_run_program_notify(self.run_program_notify)
+
+    def record(self, pid, name):
+        self.msg('pid=%d name=%s stat=%s', pid, name, self.get_sys_proc_stat(pid))
+
+    def record_this(self):
+        self.record(os.getpid(), sys.argv[0])
+
+    def run_program_notify(self, proc, prog, args, kwds):
+        self.record(proc.pid, prog)
+
+    def msg(self, msg, *args):
+        if self.fp is not None:
+            print >>self.fp, self.get_time_stamp(), msg % args
+
+    @staticmethod
+    def get_time_stamp():
+        return time.strftime('%Y-%m-%dT%H:%M:%S')
+
+    @staticmethod
+    def get_sys_proc_stat(pid):
+        sys_proc_path = FilePath('/proc/%d/stat' % (pid,))
+        if not sys_proc_path.exists():
+            return 'None'
+        with open(sys_proc_path) as fp:
+            return fp.readline()
+
