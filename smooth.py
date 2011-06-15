42d1063e95ba93365b31802d904e3dae4f3a5b04
Matt Hagy <hagy@gatech.edu>
2011-06-15 12:39:07 -0400

    Added smooth2D and used np abbreviation to refer to numpy

--- smooth.py
+++ smooth.py
@@ -1,6 +1,6 @@
 '''smoothing utility based off of scipy cookbook example
 '''
-import numpy as N
+import numpy as np
 
 windows = '''
     flat hanning hamming bartlett blackman
@@ -9,7 +9,7 @@ windows = '''
 def smooth1D(x, window_len=None, window_frac=0.05, window='hanning'):
     """smooth data using a windowing method
     """
-    x = N.asarray(x)
+    x = np.asarray(x)
     if x.ndim != 1:
         raise ValueError("array not 1 dimensional")
     if window_len is None:
@@ -20,14 +20,27 @@ def smooth1D(x, window_len=None, window_frac=0.05, window='hanning'):
         raise ValueError("invalid window")
     if window_len < 3:
         return x
-    s = N.r_[2*x[0]  - x[window_len:1:-1],
+    s = np.r_[2*x[0]  - x[window_len:1:-1],
              x,
              2*x[-1] - x[-1:-window_len:-1]]
     if window == 'flat':
-        w = N.ones(window_len, float)
+        w = np.ones(window_len, float)
     else:
-        w = getattr(N, window)(window_len)
-    y = N.convolve(w/w.sum(), s, mode='same')
+        w = getattr(np, window)(window_len)
+    y = np.convolve(w/w.sum(), s, mode='same')
     return y[window_len-1:-window_len+1]
 
+def gauss_kern2D(size, sizey=None):
+    size = int(size)
+    if not sizey:
+        sizey = size
+    else:
+        sizey = int(sizey)
+    x, y = np.mgrid[-size:size+1, -sizey:sizey+1]
+    g = np.exp(-(x**2/float(size)+y**2/float(sizey)))
+    return g / g.sum()
 
+def smooth2D(x, kernel_size, mode='same'):
+    # delay loading as scipy takes a long time to load and isn't always needed
+    from scipy import signal
+    return signal.convolve(x, gauss_kern2D(kernel_size), mode=mode, old_behavior=False)
