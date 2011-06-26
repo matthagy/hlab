41cea1d475741a5522aa1c6284cb9b759984bc76
Matt Hagy <hagy@gatech.edu>
2011-06-26 09:14:08 -0400

    Made proper use of wraps decorator and add cache as a properties to returned function

--- memorize.py
+++ memorize.py
@@ -1,13 +1,14 @@
 
 from functools import wraps
 
-@wraps
 def memorize(func):
     cache = {}
+    @wraps(func)
     def wrapper(*args):
         try:
             return cache[args]
         except KeyError:
             result = cache[args] = func(*args)
             return result
+    wrapper.cache = cache
     return wrapper
