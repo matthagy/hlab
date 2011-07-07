40b02735d1e7fa7b5f54fa7341a10c0df3c58776
Matt Hagy <hagy@gatech.edu>
2011-07-07 15:19:04 -0400

    Made memorize handle keyword arguments

--- memorize.py
+++ memorize.py
@@ -1,14 +1,36 @@
 
+from inspect import getargspec
 from functools import wraps
 
 def memorize(func):
+
     cache = {}
+
     @wraps(func)
-    def wrapper(*args):
+    def wrapper(*args, **kwds):
+
+        args, star_args, star_kwds = parse_args(func, args, kwds)
+
+        key = args, star_args, tuple((k,v) for k,v in sorted(star_kwds.iteritems()))
+
         try:
-            return cache[args]
+            return cache[key]
         except KeyError:
-            result = cache[args] = func(*args)
+            result = cache[key] = func(*(args + star_args), **star_kwds)
             return result
+
     wrapper.cache = cache
     return wrapper
+
+def parse_args(func, args, kwds):
+    a = getargspec(func)
+    n_pos = len(a.args) - len(a.defaults or ())
+    pos_args = args[:n_pos:]
+    star_args = args[len(a.args)::]
+    star_kwds = dict(zip(a.args[n_pos::], args[n_pos::]))
+    star_kwds.update(kwds)
+    pos_args = pos_args + tuple(star_kwds.pop(k) if k in star_kwds else d
+                                for k,d in zip(a.args[n_pos::],
+                                               (a.defaults or ())))
+    return pos_args, star_args, star_kwds
+
