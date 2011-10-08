e17d949e7d6f8e7f230e700b58410b93c88dea2f
Matthew Hagy <hagy@gatech.edu>
2011-10-08 07:06:17 -0400

    implemented a better 1D smoothing algorithm

--- smooth.py
+++ smooth.py
@@ -14,19 +14,39 @@ def smooth1D(x, window_len=None, window_frac=0.05, window='hanning'):
         raise ValueError("array not 1 dimensional")
     if window_len is None:
         window_len = int(round(window_frac * x.size))
+
+    if not window_len % 2:
+        window_len -= 1
+
     if x.size < window_len:
         raise ValueError("input not large enough for window size")
     if window not in windows:
         raise ValueError("invalid window")
+
     if window_len < 3:
         return x
+
     s = np.r_[2*x[0]  - x[window_len:1:-1],
              x,
              2*x[-1] - x[-1:-window_len:-1]]
+
+    s = np.r_[np.repeat(x[0], window_len - 1),
+              x,
+              np.repeat(x[-1], window_len - 1)]
+
+    s = np.r_[x[window_len-1:0:-1],
+             x,
+             x[-1:-window_len:-1]]
+
+
     if window == 'flat':
         w = np.ones(window_len, float)
     else:
         w = getattr(np, window)(window_len)
+
+    y = np.convolve(w/w.sum(), s, mode='same')
+    return y[window_len - 1 : -window_len + 1:]
+
     y = np.convolve(w/w.sum(), s, mode='same')
     return y[window_len-1:-window_len+1]
 
