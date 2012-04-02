aaf845093f4ec1394330b9404fe0358255077db9
Matt Hagy <hagy@gatech.edu>
2012-04-02 13:38:23 -0400

    Fixed binsolve to handle a target of zero

--- binsolve.py
+++ binsolve.py
@@ -4,10 +4,11 @@ def binsolve(f, target, a, b, ftol=1e-12, xtol=1e-12):
     _abs = abs
     mlast = None
     av,bv = map(f, [a,b])
+    f_scale = 1.0 / target if target != 0 else 1.0
     while True:
         m = (a+b) / 2
         mv = f(m)
-        if m==mlast or _abs((mv-target)/target) < ftol or _abs((a-b) / (0.5*(_abs(a)+_abs(b)))) < xtol:
+        if m==mlast or _abs((mv-target) * f_scale) < ftol or _abs((a-b) / (0.5*(_abs(a)+_abs(b)))) < xtol:
             return m
         if (av>target and mv>target) or (av<target and mv<target):
             a = m
